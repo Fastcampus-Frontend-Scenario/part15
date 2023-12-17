9df76d958950ae7c383eb08e49ce379d3a99b4ae
mgh <mgh@Vallsitas-MacBook-Pro-16.local>
2023-12-18 07:25:39 +0900

    refactor: UseOrderHistory Return 명세 작성

--- src/app/(order)/order-history/useOrderHistory.ts
+++ src/app/(order)/order-history/useOrderHistory.ts
@@ -1,3 +1,5 @@
+import { IOrder } from "@/types"
+
 /**
  * UseOrderHistory에 넘어오는 Props 명세
  */
@@ -9,13 +11,18 @@ interface UseOrderHistoryProps {
  * UseOrderHistory의 반환 값 명세
  */
 interface UseOrderHistoryReturn {
-
+    /** 주문 리스트 */
+    orders: IOrder[]
+    /** 주문이 비었는지 */
+    isEmpty: boolean
+    /** 주문 클릭 시 이벤트 */
+    handleClickToOrder: () => void
 }
 
 type UseOrderHistory = (props: UseOrderHistoryProps) => UseOrderHistoryReturn
 
 export const useOrderHistory: UseOrderHistory = (props)  => {
     return {
-        
+
     }
 }
