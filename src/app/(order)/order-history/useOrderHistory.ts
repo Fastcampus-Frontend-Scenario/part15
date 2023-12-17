b5f7a8830008f1b03aa1ce4da40b7d21e570f1ff
mgh <mgh@Vallsitas-MacBook-Pro-16.local>
2023-12-18 07:22:04 +0900

    refactor: useOrderHistory shape 작성

---
+++ src/app/(order)/order-history/useOrderHistory.ts
@@ -0,0 +1,21 @@
+/**
+ * UseOrderHistory에 넘어오는 Props 명세
+ */
+interface UseOrderHistoryProps {
+
+}
+
+/**
+ * UseOrderHistory의 반환 값 명세
+ */
+interface UseOrderHistoryReturn {
+
+}
+
+type UseOrderHistory = (props: UseOrderHistoryProps) => UseOrderHistoryReturn
+
+export const useOrderHistory: UseOrderHistory = (props)  => {
+    return {
+        
+    }
+}
