265339f515280ba00f50dafa47e469bd82ffb0c2
mgh <mgh@Vallsitas-MacBook-Pro-16.local>
2023-12-18 07:27:20 +0900

    refactor: useOrderHistory 완성

--- src/app/(order)/order-history/useOrderHistory.ts
+++ src/app/(order)/order-history/useOrderHistory.ts
@@ -1,4 +1,10 @@
+import useFetchCollection from "@/hooks/useFetchCollection"
+import { selectUserID } from "@/redux/slice/authSlice"
+import { STORE_ORDERS, selectOrderHistory } from "@/redux/slice/orderSlice"
 import { IOrder } from "@/types"
+import { useRouter } from "next/router"
+import { useEffect } from "react"
+import { useDispatch, useSelector } from "react-redux"
 
 /**
  * UseOrderHistory에 넘어오는 Props 명세
@@ -16,13 +22,32 @@ interface UseOrderHistoryReturn {
     /** 주문이 비었는지 */
     isEmpty: boolean
     /** 주문 클릭 시 이벤트 */
-    handleClickToOrder: () => void
+    handleClickToOrder: (id: string) => void
 }
 
 type UseOrderHistory = (props: UseOrderHistoryProps) => UseOrderHistoryReturn
 
 export const useOrderHistory: UseOrderHistory = (props)  => {
-    return {
+    const { data, isLoading } = useFetchCollection('orders');
+    const dispatch = useDispatch();
+    const router = useRouter();
 
+    useEffect(() => {
+        dispatch(STORE_ORDERS(data));
+    }, [dispatch, data]);
+
+    const orders = useSelector(selectOrderHistory);
+    const userID = useSelector(selectUserID);
+
+    const filteredOrders = orders.filter((order) => order.userID === userID);
+
+    const handleClickToOrder = (id: string) => {
+        router.push(`/order-details/${id}`)
+    }
+
+    return {
+        orders: filteredOrders,
+        isEmpty: filteredOrders.length === 0,
+        handleClickToOrder
     }
 }
