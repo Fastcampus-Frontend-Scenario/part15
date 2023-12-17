f32451154b15d181d07dc6eb836ed5c279a62051
mgh <mgh@Vallsitas-MacBook-Pro-16.local>
2023-12-18 05:22:14 +0900

    memo: components/icon/icon.tsx 수정 부분 주석 작성

--- src/components/icon/Icon.tsx
+++ src/components/icon/Icon.tsx
@@ -1,13 +1,20 @@
 import React from 'react'
 
+// =================================================
+// FIXME: 장기적으로 추가되면 길어지므로써, 코드 병목을 야기할 수 있음.
+// 또한 아이콘의 변경으로 여러 컴포넌트 디펜던시가 많아질 수 있음.
 import letterPath from './images/shape=letter.svg';
 import lockPath from './images/shape=lock.svg';
 import showPath from './images/shape=show.svg';
 import hidePath from './images/shape=hide.svg';
+// =================================================
 import Image from 'next/image';
 
 interface IIconProps {
+  // =================================================
+  // FIXME: 아이콘 제공 타입과 import 된 아이콘의 매칭이 안되서 빠지는 컴포넌트가 존재할 수 있음.
   type: 'letter' | 'lock' | 'show' | 'hide';
+  // =================================================
   alt?: string;
   [x: string]: any;
 }
@@ -15,6 +22,9 @@ interface IIconProps {
 const Icon = ({ type, alt = '', ...restProps }: IIconProps) => {
   let src = '';
 
+  // =================================================
+  // FIXME: 장기적으로 추가되면 길어지므로써, 코드 병목을 야기할 수 있음.
+  // 또한 아이콘의 변경으로 여러 컴포넌트 디펜던시가 많아질 수 있음.
   switch (type) {
     case 'letter':
       src = letterPath
@@ -31,6 +41,7 @@ const Icon = ({ type, alt = '', ...restProps }: IIconProps) => {
     default:
       throw new Error('지원하는 아이콘 타입이 존재하지 않습니다.');
   }
+  // ===================================================
 
   return <Image src={src} alt={alt} {...restProps} />
 }
